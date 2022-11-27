e737e30d661649b4fd27feb9e5885feac6208ba9
Juan Pablo Ortega Medina <jportegame@unal.edu.co>
2022-11-27 00:58:38 -0500

    lab 05: intento 2

--- pregunta.py
+++ pregunta.py
@@ -20,6 +20,7 @@ def clean_data():
 
     df = df.applymap(lambda s : s.lower().replace('_', ' ').replace('-', ' ') if type(s) == str else s)
     df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "").str.replace(",", "").str.replace("\.00", "")
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda f: datetime.strptime(f, "%Y/%m/%d") if (len(re.findall("^\d+/", f)[0]) - 1) == 4 else datetime.strptime(f, "%d/%m/%Y"))
     df = df.astype({ "monto_del_credito": int, "comuna_ciudadano": float})
 
     #
