b859a4f85e0a479ad35225f331c715ffdab238f4
Juan Pablo Ortega Medina <jportegame@unal.edu.co>
2022-11-27 00:59:36 -0500

    lab 05: intento 3

--- pregunta.py
+++ pregunta.py
@@ -16,12 +16,13 @@ def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
     df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
 
     df = df.applymap(lambda s : s.lower().replace('_', ' ').replace('-', ' ') if type(s) == str else s)
     df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "").str.replace(",", "").str.replace("\.00", "")
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda f: datetime.strptime(f, "%Y/%m/%d") if (len(re.findall("^\d+/", f)[0]) - 1) == 4 else datetime.strptime(f, "%d/%m/%Y"))
     df = df.astype({ "monto_del_credito": int, "comuna_ciudadano": float})
+
+    df.drop_duplicates(inplace = True)
 
     #
     # Inserte su código aquí
